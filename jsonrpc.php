c269f9b1cf061e5c3a011434ab303d6962dcd346
Gregory Beaver <greg@chiaraquartet.net>
2013-02-22 00:13:40 -0600

    fix path error

--- jsonrpc.php
+++ jsonrpc.php
@@ -2,7 +2,7 @@
 namespace ProxyDetector;
 include __DIR__ . '/autoload.php';
 
-Backend\DataLayer\Sqlite3::$DATABASEPATH = __DIR__ . '/data/proxies.db';
+Backend\Sqlite3::$DATABASEPATH = __DIR__ . '/data/proxies.db';
 // basic web frontend, chrome extension frontend is json.php
 $frontend = new Frontend\Json;
 $main = new Main($frontend);
